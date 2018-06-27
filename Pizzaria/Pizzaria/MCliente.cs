bb9214ca5e5af2b2c9f1590716fc615f6367f058
Isaque Kaio <32231299+kaio7@users.noreply.github.com>
2018-06-26 21:44:19 -0300

    Add files via upload

---
+++ Pizzaria/Pizzaria/MCliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizzaria
+{
+    class MCliente
+    {
+        public int IdCliente { get; set; }
+        public string Nome { get; set; }
+        public string Fone { get; set; }
+        public string Endereco { get; set; }
+    }
+}
---
+++ Pizzaria/Pizzaria/NFuncionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizzaria
+{
+    class NFuncionario
+    {
+        MFuncionario fuc = new MFuncionario();
+        List<MFuncionario> l = new List<MFuncionario>();
+
+        public void Insert(MFuncionario f)
+        {
+            MFuncionario fuc = new MFuncionario();
+            List<MFuncionario> l = new List<MFuncionario>();
+            if (fuc.Nome == "") throw new ArgumentOutOfRangeException();
+            fuc.Insert(f);
+        }
+    }
+}
